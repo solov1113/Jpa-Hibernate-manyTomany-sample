25dab9d785944a6f5dd934c990eff9661838e446
ericdev0212 <eric.devos0212@gmail.com>
2023-03-07 19:50:18 +0900

    first commit

---
+++ src/main/java/com/example/jpahibernatemanytomanysample/JpaHibernateManyTomanySampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpahibernatemanytomanysample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHibernateManyTomanySampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaHibernateManyTomanySampleApplication.class, args);
+    }
+
+}
